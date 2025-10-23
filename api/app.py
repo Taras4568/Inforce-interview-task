4ad1291a0dc8332ac8472efce43f2e20ace19240
Taras Protsiv <137615339+Taras4568@users.noreply.github.com>
2025-10-23 03:15:34 +0300

    update

--- api/app.py
+++ api/app.py
@@ -5,11 +5,11 @@ import time
 
 app = Flask(__name__)
 
-DB_HOST = os.getenv("POSTGRES_HOST", "db")
-DB_PORT = os.getenv("POSTGRES_PORT", "5432")
-DB_NAME = os.getenv("POSTGRES_DB", "testdb")
-DB_USER = os.getenv("POSTGRES_USER", "postgres")
-DB_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres")
+DB_HOST = os.getenv("APP_DB_HOST", "postgres-headless")
+DB_PORT = os.getenv("APP_DB_PORT", "5432")
+DB_USER = os.getenv("APP_DB_USER", "postgres")
+DB_PASSWORD = os.getenv("APP_DB_PASSWORD", "password")
+DB_NAME = os.getenv("APP_DB_NAME", "myapp_db")
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
